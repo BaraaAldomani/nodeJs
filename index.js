d27063702d4e425e36907f73664642c992fdcf3e
Baraa Aldomani <baraadom9@gmail.com>
2022-04-08 03:49:46 +0300

    test

---
+++ index.js
@@ -0,0 +1,11 @@
+const _ = require("underscore");
+const s = require("seedrandom");
+var random = require("random");
+const seedrandom = require("seedrandom");
+
+total = random.int((max = 10), (min = 1));
+console.log(total);
+
+
+
+
